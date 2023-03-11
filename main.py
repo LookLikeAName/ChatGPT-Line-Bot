a57e4503f686a7fd35b27e78778dd98e61fd1cc9
LookLikeAName <t106598057@ntut.edu.tw>
2023-03-11 08:11:00 +0000

    1. Add support for both individual user and group dall.e image variations.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from dotenv import load_dotenv
 from flask import Flask, request, abort
 from linebot import (LineBotApi, WebhookHandler)
-from linebot.exceptions import (InvalidSignatureError)
+from linebot.exceptions import (InvalidSignatureError, LineBotApiError)
 from linebot.models import (MessageEvent, TextMessage, ImageMessage,
                             TextSendMessage, ImageSendMessage)
 import os
 import Image
+import math
 
 from src.chatgpt import ChatGPT, DALLE
 from src.models import OpenAIModel
-from src.memory import Memory
+from src.memory import Memory, User_State, memory_user_state
 from src.logger import logger
 
 load_dotenv('.env')
@@ -17,6 +18,8 @@ load_dotenv('.env')
 app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
 handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
+
+user_state = dict()
 
 models = OpenAIModel(api_key=os.getenv('OPENAI_API'),
                      model_engine=os.getenv('OPENAI_MODEL_ENGINE'))
@@ -34,7 +37,20 @@ image_chatgpt = ChatGPT(image_models, image_memory)
 dalle = DALLE(image_models)
 
 start_key_word = os.getenv('START_KEY_WORD')
-start_key_word_1 = os.getenv('START_KEY_WORD_1')
+image_create_key_word = os.getenv('IMAGE_CREATE_KEY_WORD')
+image_variate_key_word = os.getenv('IMAGE_VARIATE_KEY_WORD')
+
+
+
+def img_resize(image: Image, size: int) -> Image:
+  max_side = image.height
+  if image.width > image.height:
+    max_side = image.width
+  ratio = float(size / max_side)
+  #print(max_side, ratio)
+  image = image.resize(
+    (math.floor(image.width * ratio), math.floor(image.height * ratio)))
+  return image
 
 
 @app.route("/callback", methods=['POST'])
@@ -62,18 +78,38 @@ def handle_text_message(event):
     push_id = group_id
     group_mode = True
 
+  print(push_id not in user_state)
+  if push_id not in user_state :
+    user_state[push_id] = User_State()
+  print(user_state[push_id].user_exist(user_id))
+  if user_state[push_id].user_exist(user_id) == False:
+    user_state[push_id].initialize(user_id)
+
+  if user_state[push_id].get_data(user_id, 0, 'state') != memory_user_state.NORMAL:
+    try:
+      line_bot_api.push_message(push_id, TextSendMessage(text="改圖中斷"))
+    except LineBotApiError as e:
+      print(e)
+    user_state[push_id].update(user_id, 0, 'state', memory_user_state.NORMAL)
   text = event.message.text
   #logger.info(f'{user_id}: {text}')
   if text.startswith(start_key_word):
     print(user_id, text)
-    if text[2:].startswith(start_key_word_1):
-      line_bot_api.push_message(push_id, TextSendMessage(text="我負責! 請稍等..."))
+    if text[2:].startswith(image_create_key_word):
+      try:
+        line_bot_api.push_message(push_id, TextSendMessage(text="我負責! 請稍等..."))
+      except LineBotApiError as e:
+        print(e)
       if text[4:].startswith("debug"):
         response = image_chatgpt.get_response(user_id, text[9:].strip())
-        line_bot_api.push_message(push_id, TextSendMessage(text=response))
+        try:
+          line_bot_api.push_message(push_id, TextSendMessage(text=response))
+        except LineBotApiError as e:
+          print(e)
       else:
         response = image_chatgpt.get_response(user_id, text[4:].strip())
       print(response)
+
       try:
         response = dalle.generate(response)
       except image_models.error.InvalidRequestError:
@@ -81,6 +117,10 @@ def handle_text_message(event):
       else:
         msg = ImageSendMessage(original_content_url=response,
                                preview_image_url=response)
+    elif text[2:].startswith(image_variate_key_word):
+      user_state[push_id].update(user_id, 0, 'state', memory_user_state.VARIATION)
+      msg = TextSendMessage(text="責任來我就扛，請傳圖片!")
+      print(push_id, user_id, user_state[push_id].get_data(user_id, 0, 'state'))
     else:
       #logger.info(f'{user_id}: {text}')
       response = chatgpt.get_response(user_id, text)
@@ -91,35 +131,57 @@ def handle_text_message(event):
     return
   line_bot_api.reply_message(event.reply_token, msg)
 
-
 @handler.add(MessageEvent, message=ImageMessage)
 def handle_image_message(event):
   user_id = event.source.user_id
+  push_id = user_id
   group_mode = False
+
   if hasattr(event.source, 'group_id'):
+    group_id = event.source.group_id
+    push_id = group_id
     group_mode = True
-  if group_mode == True:
+
+  print(push_id not in user_state)
+  if push_id not in user_state :
+    user_state[push_id] = User_State()
+  
+  print(user_state[push_id].user_exist(user_id))
+  if user_state[push_id].user_exist(user_id) == False:
+    user_state[push_id].initialize(user_id)
+
+  print(push_id, user_id, user_state[push_id].get_data(user_id, 0, 'state'))
+  if user_state[push_id].get_data(user_id, 0, 'state') == memory_user_state.NORMAL:
     return
+
   print(event.message.id)
   SendImage = line_bot_api.get_message_content(event.message.id)
   path = './img/' + event.message.id + '.png'
   with open(path, 'wb') as fd:
     for chunk in SendImage.iter_content():
       fd.write(chunk)
+  image_base = Image.new("RGBA", (256, 256))
+  image_base.save(fp="./img/mask.png")
   image_0 = Image.open(path)
-  min_side = image_0.width
-  if min_side > image_0.height:
-    min_side = image_0.height
-  image_0 = image_0.crop((0, 0, min_side, min_side))
-  image_0.thumbnail((256, 256))
-  image_0.save(fp=path)
-  line_bot_api.push_message(user_id, TextSendMessage(text="我來救! 請稍等..."))
+  image_0 = img_resize(image_0, 256)
+  start_x = 0
+  start_y = 0
+  if image_0.width > image_0.height:
+    start_y = math.floor((256 - image_0.height) / 2)
+  elif image_0.width < image_0.height:
+    start_x = math.floor((256 - image_0.width) / 2)
+  image_base.paste(image_0, (start_x, start_y))
+  image_base.save(fp=path)
+  try:
+    line_bot_api.push_message(push_id, TextSendMessage(text="我來救! 請稍等..."))
+  except LineBotApiError as e:
+    print(e)
   response = dalle.variation(path)
   msg = ImageSendMessage(original_content_url=response,
                          preview_image_url=response)
   line_bot_api.reply_message(event.reply_token, msg)
   os.remove(path)
-
+  user_state[push_id].update(user_id, 0, 'state', memory_user_state.NORMAL)
 
 @app.route("/", methods=['GET'])
 def home():
--- src/memory.py
+++ src/memory.py
@@ -1,17 +1,32 @@
 from typing import Dict
 from collections import defaultdict
-
+from enum import Enum
+ 
+class memory_user_state(Enum):
+    NORMAL = 'normal'
+    VARIATION = 'variation'
+    EDIT = 'edit'
+    RESERVED = 'reserved'
+    
 
 class MemoryInterface:
-    def append(self, user_id: str, message: Dict) -> None:
+    def append(self, user_id: str, data) -> None:
         pass
 
-    def get(self, user_id: str) -> str:
+    def update(self, user_id: str, index: int, key: str, data) -> None:
+        pass
+
+    def get_data(self, user_id: str, index: int, key: str):
+        return ""
+  
+    def get_storage(self, user_id: str):
         return ""
 
     def remove(self, user_id: str) -> None:
         pass
 
+    def user_exist(self, user_id: str) -> bool:
+      return False
 
 class Memory(MemoryInterface):
     def __init__(self, system_message):
@@ -23,17 +38,44 @@ class Memory(MemoryInterface):
             'role': 'system', 'content': self.system_message
         }]
 
-    def append(self, user_id: str, message: Dict) -> None:
+    def append(self, user_id: str, data: Dict) -> None:
         #print(user_id)
         #print(self.storage[user_id])
         if self.storage[user_id] == []:
             self.initialize(user_id)
-        self.storage[user_id].append(message)
+        self.storage[user_id].append(data)
         if(len(self.storage[user_id])) > 4 :
           del self.storage[user_id][1]
 
-    def get(self, user_id: str) -> str:
-        return self.storage[user_id]
+    def get_storage(self, user_id: str) -> str:
+      return self.storage[user_id]
 
     def remove(self, user_id: str) -> None:
-        self.storage[user_id] = []
+      self.storage[user_id] = []
+    
+    def user_exist(self, user_id: str) -> bool:
+      return (self.storage[user_id] != [])
+
+class User_State(MemoryInterface):
+  def __init__(self):
+    self.storage = defaultdict(list)
+    
+  def initialize(self, user_id: str):
+      self.storage[user_id] = [
+        {'state': memory_user_state.NORMAL, 'prompt':''}
+      ]
+
+  def update(self, user_id: str, index: int, key: str, data) -> None:
+    self.storage[user_id][index][key] = data
+
+  def get_data(self, user_id: str, index: int, key: str):
+    return self.storage[user_id][index][key]
+  
+  def get_storage(self, user_id: str):
+    return self.storage[user_id]
+  
+  def remove(self, user_id: str) -> None:
+    self.storage[user_id] = []
+
+  def user_exist(self, user_id: str) -> bool:
+    return (self.storage[user_id] != [])
